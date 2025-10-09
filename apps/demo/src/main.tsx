d85f40cf3296c16f2d73b2aae736ea09df502e8b
younghoonkim <younghoonkim@contractor.ea.com>
2025-10-10 07:43:35 +0900

    feat(demo): add prepaint

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
 import { router } from './router';
 import './index.css';
+import { createFirstTxRoot } from '@firsttx/prepaint';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,11 +15,15 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById('root')!).render(
+createFirstTxRoot(
+  document.getElementById('root')!,
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster position="top-right" expand={false} richColors closeButton />
     </QueryClientProvider>
   </StrictMode>,
+  {
+    transition: true,
+  },
 );
